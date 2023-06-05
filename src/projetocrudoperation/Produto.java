e696d6890044a8cbedbf7cfc8ea8e41e8a5889fd
Gian Carlo Silva Braga <182210120@senacinformatica.br>
2023-06-05 10:18:25 -0300

    first commit

---
+++ src/projetocrudoperation/Produto.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocrudoperation;
+
+/**
+ *
+ * @author 182210120
+ */
+public class Produto {
+    int cod;
+    String nome;
+    int qntd;
+    float preco;
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQntd() {
+        return qntd;
+    }
+
+    public void setQntd(int qntd) {
+        this.qntd = qntd;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    
+    
+    
+}
---
+++ src/projetocrudoperation/ProjetoCrudOperation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetocrudoperation;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 182210120
+ */
+public class ProjetoCrudOperation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Janela j1 = new Janela();
+        j1.setSize(715, 684);
+        j1.setVisible(true);
+        j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
